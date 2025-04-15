f433d99826bc7eb7498ca19346411e552112d803
Taodocos <148693970+Taodocos@users.noreply.github.com>
2025-04-14 23:56:57 -0700

    Update owner.php

--- owner.php
+++ owner.php
@@ -10,28 +10,33 @@
 <body>
 <section class="bg-light">
 <nav class="navbar navbar-expand-md navbar-light fixed-top border-secondary shadow-lg bg-white">
-                            <div class="container-xxl">
-                                <h1 class="navbar-brand"><strong>JONNY</strong> cars</h1>
-                            </div>
-                            <div class="collapse navbar-collapse justify-content-end" id="main-nav">
-                                <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link fw-bold" href="home.html"><strong>Home</strong></a>
-                                    </li>
-                                    <!-- <li class="nav-item">
-                                        <a class="nav-link fw-bold" href="regcarfuser.php"><strong>Cars</strong></a>
-                                    </li> -->
-                                    <!-- <li class="nav-item">
-                                        <a class="nav-link fw-bold" href="Login.php"><strong>Log in</strong></a>
-                                    </li> -->
-                                    <li class="nav-item">
-                                        <a class="nav-link fw-bold" href="about.html"><strong>About us</strong></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
+        <div class="container-xxl">
+            <a class="navbar-brand" href="regregisterdecar.php">
+            <img src="uploads/Logo.png" alt="logo" style="height: 40px;"> <!-- Adjust logo size -->
+                            <strong>Roman</strong> cars
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="main-nav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="home.html"><strong>Home</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="regcar.php"><strong>Register cars</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="about.html"><strong>About us</strong></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
                         
         <div class="container-lg my-5 justify-content-center align-items-center">
+
+        <br/><br/> <br/> <br/>
             <h3 class="fw-bold display-6 text-center ">workers of componey</h3>
             <div class="row my-12 align-items-center justify-content-center fw-light g- 5">
                 <div class="col-12 col-lg-12 col-xl-12 shadow bg-white">
