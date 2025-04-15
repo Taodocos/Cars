9705ff065d1e2a5c57720859aa65775f23aad853
Taodocos <148693970+Taodocos@users.noreply.github.com>
2025-04-14 23:42:44 -0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -29,6 +29,31 @@
 </head>
 <body>
 <section class="bg-light">
+
+	<nav class="navbar navbar-expand-md navbar-light fixed-top border-secondary shadow-lg bg-white">
+        <div class="container-xxl">
+            <a class="navbar-brand" href="about.html">
+            <img src="uploads/Logo.png" alt="logo" style="height: 40px;"> <!-- Adjust logo size -->
+                            <strong>Roman</strong> cars
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="main-nav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="home.html"><strong>Home</strong></a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link fw-bold" href="regcar.php"><strong>Register cars</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="about.html"><strong>About us</strong></a>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
         <div class="container-lg my-5 justify-content-center align-items-center">
             <h3 class="fw-bold display-6 text-center ">LOG IN</h3>
             <div class="row my-12 align-items-center justify-content-center fw-light g-3 ">
